089865c8ab0a02daa20bd413de893869150c3545
hidge123 <hidge123@outlook.com>
2026-05-12 20:03:55 +0800

    修改签到数据存储模型,将用户的credit按组存储

--- sign_config.py
+++ sign_config.py
@@ -6,7 +6,10 @@ class UserData(BaseModel):
     """单个用户的数据模型"""
     credit: int = Field(default=0, ge=0, description="用户的小饼干数量")
 
+class GroupData(BaseModel):
+    """每个消息群所有用户的数据模型"""
+    users: dict[str, UserData] = Field(default_factory=dict, description="用户字典数据")
 
 class SignData(BaseModel):
     """签到数据模型"""
-    users: dict[str, UserData] = Field(default_factory=dict, description="用户数据字典")
+    groups: dict[str, GroupData] = Field(default_factory=dict, description="群组数据字典")
